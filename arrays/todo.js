7c5dcac4e482e1c168dade9a225ab6f62c2fffd6
Jeremy Mark <jermark851@gmail.com>
2020-03-24 23:36:44 -0500

    complete array methods vid

--- arrays/todo.js
+++ arrays/todo.js
@@ -1,4 +1,8 @@
 const todos = ['walk dog', 'exercise', 'study js', 'fix car', 'laundry']
+
+todos.splice(2, 1)
+todos.push('Find a job')
+todos.shift()
+
 console.log(`You have ${todos.length} todos!`)
-console.log(todos[0])
-console.log(todos[todos.length - 2])
+console.log(todos)
