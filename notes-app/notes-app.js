54f2847d6bbde56675482db3dd2e501c50ab6b3b
jaymar85 <jermark851@gmail.com>
2020-10-14 21:01:16 -0500

    complete lesson

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,49 +1,22 @@
-let notes = []
+const notes = getSavedNotes()
 
 const filters = {
     searchText: ''
 }
 
-// Check for existing saved data
-const notesJSON = localStorage.getItem('notes')
-
-if (notesJSON !== null) {
-    notes = JSON.parse(notesJSON)
-}
-
-const renderNotes = function(notes, filters) {
-    const filteredNotes = notes.filter(function(note) {
-        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
-    })
-
-    // rerenders #notes after filter input used
-    document.querySelector('#notes').innerHTML = ''
-    
-    filteredNotes.forEach(function(note) {
-        const noteEl = document.createElement('p')
-
-        if (note.title.length > 0) {
-            noteEl.textContent = note.title
-        } else {
-            noteEl.textContent = 'Unnamed note'
-        }
-
-        document.querySelector('#notes').appendChild(noteEl)
-    })
-}
- 
 renderNotes(notes, filters)
 
-document.querySelector('#create-note').addEventListener('click', function() {
+document.querySelector('#create-note').addEventListener('click', function (e) {
     notes.push({
+        id: uuidv4(),
         title: '',
         body: ''
     })
-    localStorage.setItem('notes', JSON.stringify(notes))
-    renderNotes(notes, filters);
+    saveNotes(notes)
+    renderNotes(notes, filters)
 })
 
-document.querySelector('#search-text').addEventListener('input', function(e) {
+document.querySelector('#search-text').addEventListener('input', function (e) {
     filters.searchText = e.target.value
     renderNotes(notes, filters)
 })
